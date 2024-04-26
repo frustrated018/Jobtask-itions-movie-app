e7a67b1c7fd93ae5ef05533e12ca3b329170c389
frustrated018 <niloyd018@gmail.com>
2024-04-27 01:43:45 +0600

    Fixed bugs with search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,8 @@ import {
 import movies from "@/data/dummyData.json";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-import { FormEvent, useEffect, useState } from "react";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
@@ -36,11 +37,13 @@ export default function Home() {
 
     if (!category || !keyword) {
       toast.error("Please enter both a category and a keyword.");
+      // Resetting form if something is missing
+      setFinishedMovies(filteredMovies);
       return;
     }
 
     const newKeyword = keyword.toLocaleLowerCase();
-    
+
     // Utility function to convert an array of strings to lowercase
     const convertToLowerCase = (arr: string[]) =>
       arr.map((item) => item.toLowerCase());
@@ -67,7 +70,9 @@ export default function Home() {
     <main className="min-h-screen">
       {/* Navbar */}
       <nav className="flex justify-between items-center p-3.5 border-b">
-        <h3 className="text-4xl font-semibold">Movies</h3>
+        <Link href="/" className="text-4xl font-semibold">
+          Movies
+        </Link>
         <h5>
           Created by <span>github link</span>
         </h5>
@@ -108,98 +113,106 @@ export default function Home() {
         </form>
       </section>
 
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 pb-10 pt-5 px-5">
-        {finishedMovies.map((movie, index) => (
-          <div key={movie.imdbmovieid}>
-            <Dialog>
-              <DialogTrigger className="w-full">
-                <Card className="min-h-[500px]">
-                  <CardContent>
-                    <div className="flex items-center justify-center mt-5">
-                      <Image
-                        alt="Movie poster"
-                        src={movie?.moviemainphotos[0]}
-                        className="object-cover"
-                        height={375}
-                        width={250}
-                      />
+      {finishedMovies.length < 1 && (
+        <div className="mt-10">
+          <h2 className="text-5xl text-center ">No Movies Found!</h2>
+        </div>
+      )}
+
+      {finishedMovies.length > 0 && (
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 pb-10 pt-5 px-5">
+          {finishedMovies.map((movie, index) => (
+            <div key={movie.imdbmovieid}>
+              <Dialog>
+                <DialogTrigger className="w-full">
+                  <Card className="min-h-[500px]">
+                    <CardContent>
+                      <div className="flex items-center justify-center mt-5">
+                        <Image
+                          alt="Movie poster"
+                          src={movie?.moviemainphotos[0]}
+                          className="object-cover"
+                          height={375}
+                          width={250}
+                        />
+                      </div>
+                    </CardContent>
+                    <CardFooter>
+                      <section className="flex flex-col gap-3 w-full">
+                        <h3 className="text-lg font-semibold text-left">
+                          {movie.movietitle}
+                        </h3>
+                        <div className="flex gap-3">
+                          {movie.moviegenres.map((genre) => (
+                            <Badge key={genre}>{genre}</Badge>
+                          ))}
+                        </div>
+                      </section>
+                    </CardFooter>
+                  </Card>
+                </DialogTrigger>
+
+                <DialogContent className="bg-black max-w-4xl">
+                  <section className="grid grid-cols-2 my-5">
+                    <div className="flex justify-center items-center">
+                      <div className="relative w-[300px] h-[500px]">
+                        <Image
+                          alt="Movie poster"
+                          src={movie?.moviemainphotos[0]}
+                          className="object-cover"
+                          fill
+                        />
+                      </div>
                     </div>
-                  </CardContent>
-                  <CardFooter>
-                    <section className="flex flex-col gap-3 w-full">
-                      <h3 className="text-lg font-semibold text-left">
-                        {movie.movietitle}
-                      </h3>
-                      <div className="flex gap-3">
-                        {movie.moviegenres.map((genre) => (
-                          <Badge key={genre}>{genre}</Badge>
-                        ))}
-                      </div>
-                    </section>
-                  </CardFooter>
-                </Card>
-              </DialogTrigger>
 
-              <DialogContent className="bg-black max-w-4xl">
-                <section className="grid grid-cols-2 my-5">
-                  <div className="flex justify-center items-center">
-                    <div className="relative w-[300px] h-[500px]">
-                      <Image
-                        alt="Movie poster"
-                        src={movie?.moviemainphotos[0]}
-                        className="object-cover"
-                        fill
-                      />
+                    <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm bg-secondary">
+                      <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                          <dt className="font-medium">Title</dt>
+                          <dd className="sm:col-span-2">{movie.movietitle}</dd>
+                        </div>
+
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                          <dt className="font-medium">Available Language</dt>
+                          <dd className="sm:col-span-2">
+                            <div className="flex gap-2 flex-wrap">
+                              {movie.movielanguages.map((lang) => (
+                                <p key={lang}>{lang},</p>
+                              ))}
+                            </div>
+                          </dd>
+                        </div>
+
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                          <dt className="font-medium">Available Country</dt>
+                          <dd className="sm:col-span-2">
+                            <div className="flex gap-2 flex-wrap">
+                              {movie.moviecountries.map((country) => (
+                                <p key={country}>{country},</p>
+                              ))}
+                            </div>
+                          </dd>
+                        </div>
+
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                          <dt className="font-medium">Genre</dt>
+                          <dd className="sm:col-span-2">
+                            <div className="flex gap-2 flex-wrap">
+                              {movie.moviegenres.map((genre) => (
+                                <p key={genre}>{genre},</p>
+                              ))}
+                            </div>
+                          </dd>
+                        </div>
+                      </dl>
                     </div>
-                  </div>
-
-                  <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm bg-secondary">
-                    <dl className="-my-3 divide-y divide-gray-100 text-sm">
-                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium">Title</dt>
-                        <dd className="sm:col-span-2">{movie.movietitle}</dd>
-                      </div>
-
-                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium">Available Language</dt>
-                        <dd className="sm:col-span-2">
-                          <div className="flex gap-2 flex-wrap">
-                            {movie.movielanguages.map((lang) => (
-                              <p key={lang}>{lang},</p>
-                            ))}
-                          </div>
-                        </dd>
-                      </div>
-
-                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium">Available Country</dt>
-                        <dd className="sm:col-span-2">
-                          <div className="flex gap-2 flex-wrap">
-                            {movie.moviecountries.map((country) => (
-                              <p key={country}>{country},</p>
-                            ))}
-                          </div>
-                        </dd>
-                      </div>
-
-                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium">Genre</dt>
-                        <dd className="sm:col-span-2">
-                          <div className="flex gap-2 flex-wrap">
-                            {movie.moviegenres.map((genre) => (
-                              <p key={genre}>{genre},</p>
-                            ))}
-                          </div>
-                        </dd>
-                      </div>
-                    </dl>
-                  </div>
-                </section>
-              </DialogContent>
-            </Dialog>
-          </div>
-        ))}
-      </section>
+                  </section>
+                </DialogContent>
+              </Dialog>
+            </div>
+          ))}
+        </section>
+      )}
     </main>
   );
 }
