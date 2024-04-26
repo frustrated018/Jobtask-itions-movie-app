b2858886834408ec61c5ad3523d5e4377c961eeb
frustrated018 <niloyd018@gmail.com>
2024-04-26 22:35:09 +0600

    Finished the modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import {
-  Dialog,
-  DialogHeader,
-  DialogTrigger,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 
 import movies from "@/data/dummyData.json";
 import Image from "next/image";
@@ -35,7 +28,7 @@ export default function Home() {
               <DialogTrigger className="w-full">
                 <Card className="min-h-[500px]">
                   <CardContent>
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-center mt-5">
                       <Image
                         alt="Movie poster"
                         src={movie?.moviemainphotos[0]}
@@ -60,13 +53,67 @@ export default function Home() {
                 </Card>
               </DialogTrigger>
 
-              <DialogContent className="bg-black">
-                <DialogHeader>
-                  <DialogTitle>{movie.movietitle}</DialogTitle>
-                  <DialogDescription>
-                    Will show the details here
-                  </DialogDescription>
-                </DialogHeader>
+              <DialogContent className="bg-black max-w-4xl">
+                <section className="grid grid-cols-2 my-5">
+                  <div className="flex justify-center items-center">
+                    <div className="relative w-[300px] h-[500px]">
+                      <Image
+                        alt="Movie poster"
+                        src={movie?.moviemainphotos[0]}
+                        className="object-cover"
+                        fill
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm bg-secondary">
+                    <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">Title</dt>
+                        <dd className="text-gray-700 sm:col-span-2">
+                          {movie.movietitle}
+                        </dd>
+                      </div>
+
+                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">
+                          Available Language
+                        </dt>
+                        <dd className="text-gray-700 sm:col-span-2">
+                          <div className="flex gap-2 flex-wrap">
+                            {movie.movielanguages.map((lang) => (
+                              <p key={lang}>{lang},</p>
+                            ))}
+                          </div>
+                        </dd>
+                      </div>
+
+                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">
+                          Available Country
+                        </dt>
+                        <dd className="text-gray-700 sm:col-span-2">
+                          <div className="flex gap-2 flex-wrap">
+                            {movie.moviecountries.map((country) => (
+                              <p key={country}>{country},</p>
+                            ))}
+                          </div>
+                        </dd>
+                      </div>
+
+                      <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                        <dt className="font-medium text-gray-900">Genre</dt>
+                        <dd className="text-gray-700 sm:col-span-2">
+                          <div className="flex gap-2 flex-wrap">
+                            {movie.moviegenres.map((genre) => (
+                              <p key={genre}>{genre},</p>
+                            ))}
+                          </div>
+                        </dd>
+                      </div>
+                    </dl>
+                  </div>
+                </section>
               </DialogContent>
             </Dialog>
           </div>
